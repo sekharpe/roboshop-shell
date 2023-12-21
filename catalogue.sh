1b3808eb23525948f4ed6c82bcf45bbbebc9d2b3
sekharpe <psekhar93440@gmail.com>
2023-12-21 17:25:17 +0530

    adding mongodb files

--- catalogue.sh
+++ catalogue.sh
@@ -24,47 +24,47 @@ VALIDATE(){
     fi
 }
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>> $LOGFILE
 VALIDATE $? "the nodejs module old version disabled"
 
-dnf module enable nodejs:18 -y
+dnf module enable nodejs:18 -y &>> $LOGFILE
 VALIDATE $? "the nodejs module new version disabled"
 
-dnf install nodejs -y
+dnf install nodejs -y &>> $LOGFILE
 VALIDATE $? "the nodejs installation"
 
-useradd roboshop
+useradd roboshop &>> $LOGFILE
 VALIDATE $? "the user roboshop added"
 
-mkdir /app
+mkdir /app &>> $LOGFILE
 VALIDATE $? "the application app directory created"
 
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
 VALIDATE $? "Downloaded code to the application app directory"
 
 cd /app 
-npm install 
+npm install &>> $LOGFILE
 VALIDATE $? "installing the npm dependencies"
 
-cp catalogue.service /etc/systemd/system/
+cp catalogue.service /etc/systemd/system/ &>> $LOGFILE
 VALIDATE $? "catalogue service is created in /etc/systemd/system"
 
-systemctl daemon-reload
+systemctl daemon-reload &>> $LOGFILE
 VALIDATE $? "catalogue service is loaded"
 
-systemctl enable catalogue
+systemctl enable catalogue &>> $LOGFILE
 VALIDATE $? "catalogue service is enabled now"
 
-systemctl start catalogue
+systemctl start catalogue &>> $LOGFILE
 VALIDATE $? "catalogue service is started now"
 
-cp mongo.repo /etc/yum.repos.d/
+cp mongo.repo /etc/yum.repos.d/ &>> $LOGFILE
 VALIDATE $? "mongo.repo added to /etc/yum.repos"
 
-dnf install mongodb-org-shell -y
+dnf install mongodb-org-shell -y &>> $LOGFILE
 VALIDATE $? "mongodb client installation to push content to db"
 
-mongo --host mongodb.pghub.online </app/schema/catalogue.js
+mongo --host mongodb.pghub.online </app/schema/catalogue.js &>> $LOGFILE
 VALIDATE $? "loading the data to the DB"
 
 
