1d7d2cc7a14b9d4530b04d390cf6cd4988d1e824
sekharpe <psekhar93440@gmail.com>
2023-12-21 16:59:38 +0530

    adding mongodb files

---
+++ mongodb.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+ID=$(id -u)
+
+TIMESTAMP=$(date -%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+if [ $ID -ne 0 ]
+then
+echo -e "please switch to root user"
+else
+echo -e "you are the root user to run the sript"
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$2 is $R FAILED $N"
+    else
+    echo -e "$2 is $G SUCCESS $N"
+}
+
+cp mongo.repo /etc/yum.repos.d
+VALIDATE $? "the mongo.repo has been added to the /etc/yum.repos folder"
+
+dnf install mongodb-org -y &>> $LOGFILE
+VALIDATE $? "the mongodb installation"
+
+systemctl enable mongod &>> $LOGFILE
+VALIDATE $? "the mongodb is enabled now"
+
+systemctl start mongod &>> $LOGFILE
+VALIDATE $? "the mongodb is started "
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+VALIDATE $? "the mongodb port 0.0.0.0 has been changed "
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "the mongod is restarted"
+
